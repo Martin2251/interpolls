b681f4ae45c2233e3d1d6ee3e4c15b0bc0f8caf8
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-05-24 07:30:20 +0100

    before caching issues

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,48 +1,44 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import { useEffect, useState } from 'react'
-export interface Notices{
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { useEffect, useState } from "react";
+export interface Notices {
   forename: string;
-  date_of_birth:string;
-  entity_id:string;
-  nationalities:string[];
-  name:string;
-  _links:   Links;
+  date_of_birth: string;
+  entity_id: string;
+  nationalities: string[];
+  name: string;
+  _links: Links;
 }
 
-export interface Links{
-  self:Images;
-  images:Images;
-  thumbnail:Images;
+export interface Links {
+  self: Images;
+  images: Images;
+  thumbnail: Images;
 }
 
-export interface Images{
-  href:string;
+export interface Images {
+  href: string;
 }
 
 const Home: NextPage = () => {
+  const [notices, setNotices] = useState<Notices[]>([]);
+  const [search, setSearch] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
 
-  const [notices,setNotices] = useState([])
-  const [search, setSearch] = useState<string | null>(null)
-  const [loading, setLoading] = useState(false)
-
-
-useEffect(() =>{
-// search the api 
-async function fetchData(){
-  setLoading(true)
-  const data = await fetch(`https://ws-public.interpol.int/notices/v1/red?forename=${search}&resultPerPage=200`)
-  .then((res) => res.json());
-  setNotices(data._embedded.notices)
-  setLoading(false)
-}
-fetchData()
-
-},[search])
-
-
+  useEffect(() => {
+    // search the api
+    async function fetchData() {
+      setLoading(true);
+      const data = await fetch(
+        `https://ws-public.interpol.int/notices/v1/red?forename=${search}&resultPerPage=200`
+      ).then((res) => res.json());
+      setNotices(data._embedded.notices);
+      setLoading(false);
+    }
+    fetchData();
+  }, [search]);
 
   return (
     <div className={styles.container}>
@@ -53,11 +49,28 @@ fetchData()
       </Head>
 
       <main>
-        <input type="search" placeholder='search' onChange={(e)=>setSearch(e.target.value)}  />
-        {JSON.stringify(notices[1])}
+        <input
+          type="search"
+          placeholder="search"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        {notices.map((notice) => {
+          return (
+            <div key={notice.entity_id} className={styles.notice}>
+              {notice._links?.thumbnail.href && (
+                <Image
+                  src={notice._links.thumbnail.href}
+                  width="100px"
+                  height="100px"
+                  alt={notice.name}
+                />
+              )}
+            </div>
+          );
+        })}
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
